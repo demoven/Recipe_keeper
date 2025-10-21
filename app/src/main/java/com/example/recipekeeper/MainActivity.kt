fcb1c955684504d071b6a3603ac98d3875469c67
Flavien B <flavien.baron@epita.fr>
2025-10-21 15:47:35 -0400

    test push

--- app/src/main/java/com/example/recipekeeper/MainActivity.kt
+++ app/src/main/java/com/example/recipekeeper/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Heeeeeello $name!",
         modifier = modifier
     )
 }
