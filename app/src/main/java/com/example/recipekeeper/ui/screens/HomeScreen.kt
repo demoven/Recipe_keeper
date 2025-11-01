e3d78dbcbc515927c6926504a77a25d1b5f57e91
Théo FERNANDO <theo.fernando@isen.yncrea.fr>
2025-11-01 18:14:24 -0400

    Feat: list of cards

--- app/src/main/java/com/example/recipekeeper/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/recipekeeper/ui/screens/HomeScreen.kt
@@ -1,10 +1,76 @@
 package com.example.recipekeeper.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.example.recipekeeper.RecipeKeeperScreen
+import com.example.recipekeeper.R
 
 @Composable
 fun HomeScreen(onNavigate: (RecipeKeeperScreen) -> Unit) {
-    Text("Page d'accueil ")
+    Column(
+        modifier = Modifier.padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        // Première ligne : deux cartes
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            CardField(modifier = Modifier.weight(1f))
+            CardField(modifier = Modifier.weight(1f))
+        }
+
+        // Deuxième ligne : une carte à gauche, colonne de droite vide
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            CardField(modifier = Modifier.weight(1f))
+            Spacer(modifier = Modifier.weight(1f)) // colonne vide à droite
+        }
+    }
+}
+
+@Composable
+fun CardField(
+    modifier: Modifier = Modifier
+) {
+    val cardShape = RoundedCornerShape(8.dp)
+
+    Card(
+        modifier = modifier,
+        shape = cardShape
+    ) {
+        Column(modifier = Modifier.fillMaxWidth()) {
+            Image(
+                painter = painterResource(R.drawable.logo_open_no_bg),
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(180.dp)
+                    .clip(RoundedCornerShape(topStart = 8.dp, topEnd = 8.dp)),
+                contentScale = ContentScale.Crop
+            )
+
+            Text(
+                text = "Titre de la recette",
+                modifier = Modifier.padding(start = 12.dp, end = 12.dp, top = 8.dp, bottom = 12.dp)
+            )
+        }
+    }
 }
