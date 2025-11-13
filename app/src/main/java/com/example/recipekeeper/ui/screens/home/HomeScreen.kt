de8e55f2b8df711d1935d6061f434a32e9a632ff
Théo FERNANDO <theo.fernando@isen.yncrea.fr>
2025-11-13 15:15:03 -0500

    fix: unused imports

--- app/src/main/java/com/example/recipekeeper/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/recipekeeper/ui/screens/home/HomeScreen.kt
@@ -1,6 +1,5 @@
 package com.example.recipekeeper.ui.screens.home
 
-import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
@@ -38,7 +37,7 @@ fun HomeScreen(
         homeViewModel.getFolders(folderId)
         homeViewModel.getRecipes(folderId)
     }
-    
+
     LazyVerticalGrid(
         columns = GridCells.Fixed(2),
         modifier = modifier,
