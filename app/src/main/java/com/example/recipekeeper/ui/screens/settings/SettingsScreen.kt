f2edc24c8de5cc5a1d9d361cb1e1234007bd6af8
Théo FERNANDO <theo.fernando@isen.yncrea.fr>
2025-11-29 00:17:17 -0500

    feat: new fields for account settings

--- app/src/main/java/com/example/recipekeeper/ui/screens/settings/SettingsScreen.kt
+++ app/src/main/java/com/example/recipekeeper/ui/screens/settings/SettingsScreen.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
@@ -16,14 +15,12 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ExitToApp
 import androidx.compose.material.icons.outlined.Delete
-import androidx.compose.material.icons.outlined.Email
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -34,129 +31,198 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.recipekeeper.R
 import com.example.recipekeeper.ui.screens.auth.EmailTextField
+import com.example.recipekeeper.ui.screens.auth.PasswordTextField
 
 @Composable
 fun SettingsScreen(
     onLogout: () -> Unit
 ) {
-    var email by remember { mutableStateOf("") }
-    var password by remember { mutableStateOf("") }
-
     Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(dimensionResource(R.dimen.padding_medium))
-            .verticalScroll(rememberScrollState())
+            .verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_medium))
     ) {
-        Card {
-            Column (
-                modifier = Modifier
-                    .padding(dimensionResource(R.dimen.padding_large)),
-                verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_medium))
+
+        EmailCard(
+            onEmailUpdate = { /* TODO */ }
+        )
+
+        SecurityCard(
+            onPasswordChange = { /* TODO */ }
+        )
+
+        DeletionAccountCard(
+            onDeleteAccount = { /* TODO */ }
+        )
+
+        SectionLogout(
+            onLogout = onLogout
+        )
+    }
+}
+
+@Composable
+fun EmailCard(
+    onEmailUpdate: (String) -> Unit
+) {
+    var email by remember { mutableStateOf("") }
+    var currentPassword by remember { mutableStateOf("") }
+    Card {
+        Column (
+            modifier = Modifier
+                .padding(dimensionResource(R.dimen.padding_large)),
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_medium))
+        ) {
+            Text(
+                text = stringResource(R.string.account).uppercase(),
+                style = MaterialTheme.typography.titleMedium,
+            )
+
+            PasswordTextField(
+                label = stringResource(R.string.current_password),
+                password = currentPassword,
+                passwordError = false,
+                passwordErrorMessage = null,
+                onPasswordChanged = {},
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            EmailTextField(
+                email = email,
+                emailError = false,
+                onEmailChanged = { email = it },
+                modifier = Modifier.fillMaxWidth(),
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    imeAction = ImeAction.Done
+                ),
+                keyboardActions = null
+            )
+
+            Button(
+                onClick = { onEmailUpdate },
+                modifier = Modifier.fillMaxWidth()
             ) {
-                Text(
-                    text = "COMPTE",
-                    style = MaterialTheme.typography.titleMedium,
-                )
+                Text(stringResource(R.string.update_email))
+            }
 
-                EmailTextField(
-                    email = email,
-                    emailError = false,
-                    onEmailChanged = { email = it },
-                    modifier = Modifier.fillMaxWidth(),
-                    keyboardOptions = KeyboardOptions.Default.copy(
-                        imeAction = ImeAction.Done
-                    ),
-                    keyboardActions = null
-                )
+        }
+    }
+}
 
-                Button(
-                    onClick = {},
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    Text("Mettre à jour l'email")
-                }
+@Composable
+fun SecurityCard(
+    onPasswordChange: () -> Unit
+) {
+    var newPassword by remember { mutableStateOf("") }
+    var confirmNewPassword by remember { mutableStateOf("") }
+    Card {
+        Column(
+            modifier = Modifier
+                .padding(dimensionResource(R.dimen.padding_large)),
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_medium))
+        ) {
+            // --- Section Mot de passe ---
+            Text(
+                text = stringResource(R.string.security).uppercase(),
+                style = MaterialTheme.typography.titleMedium,
+            )
+
+            // Champ 1 : Nouveau mot de passe
+            PasswordTextField(
+                label = stringResource(R.string.current_password),
+                password = newPassword,
+                passwordError = false,
+                passwordErrorMessage = null,
+                onPasswordChanged = { newPassword = it },
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            // Champ 2 : Confirmer le mot de passe
+            PasswordTextField(
+                label = stringResource(R.string.new_password),
+                password = confirmNewPassword,
+                passwordError = false,
+                passwordErrorMessage = null,
+                onPasswordChanged = { confirmNewPassword = it },
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Button(
+                onClick = { onPasswordChange },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(stringResource(R.string.update_password))
             }
         }
+    }
+}
 
-        Spacer(modifier = Modifier.height(24.dp))
-
-        // Section: Modifier le mot de passe
-        Text("Modifier le mot de passe", fontWeight = FontWeight.Bold, fontSize = 18.sp)
-        Spacer(modifier = Modifier.height(8.dp))
-        //OutlinedTextField(
-        //    modifier = Modifier.fillMaxWidth(),
-        //    value = password,
-        //    onValueChange = { password = it },
-        //    label = { Text("Nouveau mot de passe") },
-        //    visualTransformation = PasswordVisualTransformation()
-        //)
-        //Spacer(modifier = Modifier.height(8.dp))
-        Button(
-            onClick = { /* a remplir avec la logique de modification */ },
-            modifier = Modifier.align(Alignment.CenterHorizontally)
+@Composable
+fun DeletionAccountCard(
+    onDeleteAccount: () -> Unit
+) {
+    Card {
+        Column(
+            modifier = Modifier
+                .padding(dimensionResource(R.dimen.padding_large)),
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_medium))
         ) {
-            Text("Modifier le mot de passe")
-        }
+            Text(
+                text = stringResource(R.string.deletion_account).uppercase(),
+                style = MaterialTheme.typography.titleMedium,
+            )
 
-        Spacer(modifier = Modifier.height(24.dp))
+            Text(
+                text = stringResource(R.string.deletion_account_confirmation),
+                style = MaterialTheme.typography.bodyLarge
+            )
 
-      Card() {
-          Column(
-              modifier = Modifier
-                  .padding(dimensionResource(R.dimen.padding_large)),
-              verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_medium))
-          ) {
-              Text(
-                  text = "SUPPRESSION DU COMPTE",
-                    style = MaterialTheme.typography.titleMedium,
-              )
-
-              Text(
-                  text = "La suppression de votre compte est irréversible. Toutes vos données seront perdues.",
-                  style = MaterialTheme.typography.bodyLarge
-              )
-
-              OutlinedButton(
-                  onClick = {},
-                  border = BorderStroke(1.dp, MaterialTheme.colorScheme.error),
-                  colors = ButtonDefaults.outlinedButtonColors(
-                      contentColor = MaterialTheme.colorScheme.error
-                  ),
-                  modifier = Modifier.fillMaxWidth()
-              ) {
-                  Icon(
-                      imageVector = Icons.Outlined.Delete,
-                        contentDescription = null
-                  )
-                  Spacer(modifier = Modifier.width(8.dp))
-                  Text("Supprimer le compte")
-              }
-          }
-      }
-
-        Spacer(modifier = Modifier.height(32.dp))
-
-        Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
-            TextButton(
-                onClick = onLogout
+            OutlinedButton(
+                onClick = onDeleteAccount,
+                border = BorderStroke(1.dp, MaterialTheme.colorScheme.error),
+                colors = ButtonDefaults.outlinedButtonColors(
+                    contentColor = MaterialTheme.colorScheme.error
+                ),
+                modifier = Modifier.fillMaxWidth()
             ) {
                 Icon(
-                    imageVector = Icons.AutoMirrored.Filled.ExitToApp,
+                    imageVector = Icons.Outlined.Delete,
                     contentDescription = null
                 )
-                Spacer(modifier = Modifier.padding(start = 8.dp))
-                Text(
-                    text = "Se déconnecter",
-                    style = MaterialTheme.typography.titleLarge
-                    )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(stringResource(R.string.delete_account))
             }
+        }
+    }
+}
+
+@Composable
+fun SectionLogout(
+    onLogout: () -> Unit
+) {
+    Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
+        TextButton(
+            onClick = onLogout
+        ) {
+            Icon(
+                imageVector = Icons.AutoMirrored.Filled.ExitToApp,
+                contentDescription = null
+            )
+            Spacer(modifier = Modifier.padding(start = 8.dp))
+            Text(
+                text = stringResource(R.string.logout),
+                style = MaterialTheme.typography.titleLarge
+                )
         }
     }
 }
